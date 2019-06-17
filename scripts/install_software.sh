0f1b1b6bd2038703085963d4058c06eddb49d330
rohitgabriel <rohit.gabriel@gmail.com>
2019-06-17 14:43:06 +1200

    updated ami id

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum -y update
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 #yum install ansible
 
