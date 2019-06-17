1c82574a6f2e70db435d3ea61cf1e91d45f667a4
rohitgabriel <rohit.gabriel@gmail.com>
2019-06-17 14:19:13 +1200

    updated keys

--- key.tf
+++ key.tf
@@ -3,3 +3,7 @@ resource "aws_key_pair" "ssh-test-key2" {
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
+resource "aws_key_pair" "ssh-test-key" {
+  key_name   = "ssh-test-key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY2)
+}
