5a56bdd87824e542644cac7ec7cc909cbd9d934c
rohitgabriel <rohit.gabriel@gmail.com>
2019-07-11 16:10:43 +1200

    adding helm to the mix

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -3,3 +3,7 @@ yum -y update
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 #yum install ansible
 
+curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
